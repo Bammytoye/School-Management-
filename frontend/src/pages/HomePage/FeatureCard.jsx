9650c829a091ccecea957851596c96672a04411b
bammytoye <bamigbalatoyese@gmail.com>
2026-03-11 16:19:12 +0000

    'homepageResponsivness'

--- frontend/src/pages/HomePage/FeatureCard.jsx
+++ frontend/src/pages/HomePage/FeatureCard.jsx
@@ -1,4 +1,4 @@
-import { useVisible } from '../../hooks/useVisible'
+import { useVisible } from "../../hooks/UseVisible";
 import { FaUserGraduate, FaBook, FaBullseye, FaChartBar, FaCalendarAlt, FaClipboard } from 'react-icons/fa'
 
 const iconMap = {
